43a2b424accdc9d0528d81cff5784e0db95d01e5
joshuachristopher <joshua.christopher@gmail.com>
2026-09-13 01:16:33 +0000

    Add basic setup for IaC

---
+++ infrastructure/main.tf
@@ -0,0 +1,64 @@
+resource "azurerm_resource_group" "example" {
+  name     = "example-group"
+  location = "West Europe"
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = "examplesa"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_service_plan" "example" {
+  name                = "example-service-plan"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  os_type             = "Linux"
+  sku_name            = "S1"
+}
+
+resource "azurerm_linux_function_app" "example" {
+  name                = "example-function-app"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  service_plan_id     = azurerm_service_plan.example.id
+
+  storage_account_name       = azurerm_storage_account.example.name
+  storage_account_access_key = azurerm_storage_account.example.primary_access_key
+
+  site_config {
+    application_stack {
+      python_version = "3.9"
+    }
+  }
+}
+
+resource "azurerm_function_app_function" "example" {
+  name            = "example-function-app-function"
+  function_app_id = azurerm_linux_function_app.example.id
+  language        = "Python"
+  test_data = jsonencode({
+    "name" = "Azure"
+  })
+  config_json = jsonencode({
+    "bindings" = [
+      {
+        "authLevel" = "function"
+        "direction" = "in"
+        "methods" = [
+          "get",
+          "post",
+        ]
+        "name" = "req"
+        "type" = "httpTrigger"
+      },
+      {
+        "direction" = "out"
+        "name"      = "$return"
+        "type"      = "http"
+      },
+    ]
+  })
+}
